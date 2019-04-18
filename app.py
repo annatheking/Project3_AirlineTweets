2c43d4877a0dede5b3657b7cf3b263c0e8eb7439
unknown <shuhua_q@hotmail.com>
2019-04-18 15:10:53 -0700

    bubble chart updates

---
+++ app.py
@@ -0,0 +1,249 @@
+import os
+import json
+
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from datetime import datetime
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine,func
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, render_template
+from flask import request
+import re
+
+from wordcloud import STOPWORDS
+import collections
+stopwords = set(STOPWORDS)
+
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:kaba.611@localhost:3306/airlinetwitter"
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Tweets = Base.classes.tweets
+
+def wordCloud(tweet_words):
+    '''
+    Tokenize the words of each tweet to generate Word Cloud 
+    '''
+    word_tokens = tweet_words.split()
+    filtered_sentence = [w for w in word_tokens if not w.lower() in stopwords]
+    wordcount=[]
+    myWord= {}
+    for word in filtered_sentence:
+        if re.match(r'^\w+$', word) and not word.isdigit():
+            if word.lower() in myWord:
+                myWord[word.lower()] += 1
+            else:
+                myWord[word.lower()] = 1 
+    for k,v in myWord.items():
+        item={} 
+        item['text']=k
+        item['weight']=v
+        wordcount.append(item)
+    return wordcount
+ 
+def searchQuery(searchParam,charttype):
+    '''
+     Create search query based on chart type
+    '''
+    try:
+        # Query the Table
+        airline= searchParam['airline']
+        tweet=searchParam['tweet']
+        # ----------------------------------
+        if charttype=='data':
+            query=db.session.query(Tweets)
+            if airline=='All' and tweet=='':
+                #Only top 50 rows
+                query=query.limit(50)
+            if airline!='All':
+                query = query.filter(Tweets.airline==airline)
+            if tweet:
+                query = query.filter(Tweets.text.contains(tweet))
+        elif charttype=='wordcloud':
+            query=db.session.query(Tweets.text)
+            if airline!='All':
+                query = query.filter(Tweets.airline==airline) 
+            if tweet:
+                query = query.filter(Tweets.text.contains(tweet))      
+        elif charttype=='pie':
+            query=db.session.query(Tweets.airline_sentiment,func.count(Tweets.airline_sentiment).label('count')) 
+            if airline!='All':
+                query = query.filter(Tweets.airline==airline) 
+            if tweet:
+                query = query.filter(Tweets.text.contains(tweet))
+            query=query.group_by(Tweets.airline_sentiment)
+        elif charttype=='bar':
+            query=db.session.query(Tweets.airline,Tweets.airline_sentiment,
+                            func.count(Tweets.airline_sentiment).label('count')) 
+            if airline!='All':
+                query = query.filter(Tweets.airline==airline) 
+            if tweet:
+                query = query.filter(Tweets.text.contains(tweet))
+            query=query.group_by(Tweets.airline,Tweets.airline_sentiment)
+        elif charttype=='line':     
+            query=db.session.query(Tweets.airline,Tweets.airline_sentiment,Tweets.tweet_date,
+                                                           func.count(Tweets.airline_sentiment).label('count')) 
+            if airline!='All':
+                query=query.filter(Tweets.airline==airline) 
+            if tweet:
+                query = query.filter(Tweets.text.contains(tweet))
+            query=query.group_by(Tweets.tweet_date,Tweets.airline_sentiment,Tweets.airline)
+            query=query.order_by(Tweets.tweet_date.desc())
+
+    except:
+        # Return some sample data in case of error
+        query = db.session.query(Tweets).limit(25)
+    return query
+
+'''
+Data
+'''
+@app.route('/api/data', methods=['GET'])
+def data():
+    searchParam = request.args.to_dict()
+    #Data
+    results=searchQuery(searchParam,'data').all()
+    all_tweets = []
+    tweet_words=''
+    for result in results:
+        tweet= {}
+        tweet["sentiment"] = result.airline_sentiment
+        tweet["airline"] = result.airline
+        tweet["tweet"] = result.text
+        tweet["date"] = result.tweet_date
+        tweet["lat"] = float(result.lat) 
+        tweet["lng"] = float(result.lng) 
+        tweet_words+=result.text
+        all_tweets.append(tweet)
+    # Format the data to send as json
+    return jsonify(all_tweets=all_tweets)
+
+'''
+Word Cloud
+'''
+@app.route('/api/wordcloud', methods=['GET'])
+def wordcloud():
+    searchParam = request.args.to_dict()
+    #Word Cloud
+    results=searchQuery(searchParam,'wordcloud').all()
+    tweet_words=''.join([r.text for r in results])
+    wordcloud_data=wordCloud(tweet_words)
+    # Format the data to send as json
+    return jsonify(wordcloud_data=wordcloud_data)
+
+'''
+Pie Chart
+'''
+@app.route('/api/pie', methods=['GET'])
+def pie():
+    searchParam = request.args.to_dict()
+    #Pie chart
+    results=searchQuery(searchParam,'pie').all()
+    piechart_data=[]
+    for result in results:
+        tweet= {}
+        tweet["sentiment"] = result.airline_sentiment
+        tweet["count"] = result.count
+        piechart_data.append(tweet)
+    # Format the data to send as json
+    return jsonify(piechart_data=piechart_data)  
+ 
+'''
+Bar Chart
+'''
+@app.route('/api/bar', methods=['GET'])
+def bar():  
+    searchParam = request.args.to_dict()
+    #Bar chart
+    results=searchQuery(searchParam,'bar').all()
+    barchart_data=[]
+    for result in results:
+        tweet= {}
+        tweet["sentiment"] = result.airline_sentiment
+        tweet["airline"] = result.airline
+        tweet["count"] = result.count
+        barchart_data.append(tweet)
+    # Format the data to send as json
+    return jsonify(barchart_data=barchart_data)
+'''
+Line Chart
+'''
+@app.route('/api/line', methods=['GET'])
+def line():     
+    searchParam = request.args.to_dict()
+    #line chart
+    results=searchQuery(searchParam,'line').all()
+    linechart_data=[]
+    for result in results:
+        try:
+            if datetime.strptime(str(result.tweet_date), '%m/%d/%Y'):
+                tweet= {}
+                tweet["sentiment"] = result.airline_sentiment
+                tweet["airline"] = result.airline
+                tweet["date"] = result.tweet_date
+                tweet["count"] = result.count
+                linechart_data.append(tweet)
+        except ValueError:
+             pass
+    return jsonify(linechart_data=linechart_data)
+'''
+Bubble Chart
+'''
+@app.route('/api/bubble', methods=['GET'])
+def bubble():
+    result=db.engine.execute("call sp_bubblechart()").fetchall()
+    return jsonify({'bubblechart_data': [dict(row) for row in result]})
+
+
+'''
+Map 
+'''
+@app.route('/api/map/', methods=['GET'])
+def mapapi():
+    #Map
+    query=db.session.query(Tweets.airline,Tweets.airline_sentiment,Tweets.text,
+                               Tweets.lat,Tweets.lng).filter(Tweets.lat!=0).filter(Tweets.lng!=0) 
+    results=query.all()
+    map_data = []
+    for result in results:
+        tweet= {}
+        tweet["sentiment"] = result.airline_sentiment
+        tweet["airline"] = result.airline
+        tweet["text"] = result.text
+        tweet["lat"] = float(result.lat) 
+        tweet["lng"] = float(result.lng) 
+        map_data.append(tweet)
+    return jsonify(map_data=map_data)
+ 
+            
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
---
+++ static/js/app.js
@@ -0,0 +1,339 @@
+var submit = d3.select("#filter-btn");
+var table = d3.select("#tweet-table");
+var tbody = d3.select("#tweet-table tbody");
+
+$(document).ajaxStart(function () {
+  $("#loaddiv").show();
+});
+
+$(document).ajaxComplete(function () {
+  $("#loaddiv").hide();
+});
+
+/**
+ * Initialize 
+ */
+function init() {
+  d3.select("#alertError").attr("class", "alert alert-danger alert-dismissable hide in fade");
+  d3.select("#alertSuccess").attr("class", "alert alert-success alert-dismissable hide in fade");
+  tbody.html('');
+  apiCall('All', '');
+}
+/**
+ *  API Call
+ */
+function apiCall(airlineValue, tweetValue) {
+
+  $("#loaddivData").show();
+  $.getJSON('/api/data', {
+    airline: airlineValue,
+    tweet: tweetValue
+  }, function (data) {
+    populateData(data.all_tweets);
+    $("#loaddivData").hide();
+  });
+
+  $("#loaddivWord").show();
+  $.getJSON('/api/wordcloud', {
+    airline: airlineValue,
+    tweet: tweetValue
+  }, function (data) {
+    wordcloud(data.wordcloud_data);
+    $("#loaddivWord").hide();
+  });
+
+  $("#loaddivPie").show();
+  $.getJSON('/api/pie', {
+    airline: airlineValue,
+    tweet: tweetValue
+  }, function (data) {
+    pieChart(data.piechart_data);
+    $("#loaddivPie").hide();
+  });
+
+  $("#loaddivBar").show();
+  $.getJSON('/api/bar', {
+    airline: airlineValue,
+    tweet: tweetValue
+  }, function (data) {
+    barChart(data.barchart_data);
+    $("#loaddivBar").hide();
+  });
+
+  $("#loaddivLine").show();
+  $.getJSON('/api/line', {
+    airline: airlineValue,
+    tweet: tweetValue
+  }, function (data) {
+    lineChart(data.linechart_data);
+    $("#loaddivLine").hide();
+  });
+
+  $("#loaddivBubble").show();
+  $.getJSON('/api/bubble', {
+    airline: airlineValue,
+    tweet: tweetValue
+  }, function (data) {
+    bubbleChart(data.bubblechart_data);
+    $("#loaddivBubble").hide();
+  });
+}
+/**
+ * Word Cloud
+ */
+function wordcloud(wordsData) {
+  /*
+  / Generate Word Cloud from tweet words 
+  */
+  d3.select('#wordcloud').html('');
+  wordsData.forEach(function (v) { delete v.attr });
+  if ($('#wordcloud').hasClass("jqcloud")) {
+    $('#wordcloud').jQCloud('update', wordsData);
+  }
+  else {
+    $('#wordcloud').jQCloud(wordsData, {
+      delay: 10,
+      autoResize: true,
+      fontSize: {
+        from: 0.6,
+        to: 0.03
+      }
+    });
+  }
+}
+/**
+ * Pie Chart
+ */
+function pieChart(pieData) {
+  //Pie chart
+  data = [{
+    labels: pieData.map(t => t.sentiment),
+    values: pieData.map(t => t.count),
+    text: pieData.map(t => t.sentiment),
+    hoverinfo: 'text+percent',
+    textinfo: 'text+percent',
+    type: 'pie'
+  }];
+  var layout = {
+    hovermode: 'closest',
+    margin: { t: 0 },
+    height: 550,
+    width: 650
+  };
+  Plotly.newPlot("piechart", data, layout);
+}
+
+/**
+ * Bar Chart
+ */
+function barChart(barData) {
+  airline = [...new Set(barData.map(t => t.airline))];
+  var trace1 = {
+    x: airline,
+    y: barData.filter(t => t.sentiment == 'negative').map(t => t.count),
+    name: 'negative',
+    type: 'bar'
+  };
+
+  var trace2 = {
+    x: airline,
+    y: barData.filter(t => t.sentiment == 'positive').map(t => t.count),
+    name: 'positive',
+    type: 'bar'
+  };
+
+  var trace3 = {
+    x: airline,
+    y: barData.filter(t => t.sentiment == 'neutral').map(t => t.count),
+    name: 'neutral',
+    type: 'bar'
+  };
+
+  var data = [trace1, trace2, trace3];
+
+  var layout = {
+    height: 550,
+    width: 650,
+    barmode: 'stack',
+    xaxis: {
+      title: {
+        text: 'Airline',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Tweet count',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+
+  Plotly.newPlot('barchart', data, layout);
+}
+
+/**
+ * Line Chart
+ */
+function lineChart(lineData) {
+  var trace1 = {
+    x: lineData.filter(t => t.sentiment == 'negative').map(t => t.date),
+    y: lineData.filter(t => t.sentiment == 'negative').map(t => t.count),
+    name: 'negative',
+    type: 'line'
+  };
+
+  var trace2 = {
+    x: lineData.filter(t => t.sentiment == 'positive').map(t => t.date),
+    y: lineData.filter(t => t.sentiment == 'positive').map(t => t.count),
+    name: 'positive',
+    type: 'line'
+  };
+
+  var trace3 = {
+    x: lineData.filter(t => t.sentiment == 'neutral').map(t => t.date),
+    y: lineData.filter(t => t.sentiment == 'neutral').map(t => t.count),
+    name: 'neutral',
+    type: 'line'
+  };
+
+  var data = [trace1, trace2, trace3];
+  var layout = {
+    height: 550,
+    width: 650,
+    xaxis: {
+      title: {
+        text: 'Date tweet',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      },
+    },
+    yaxis: {
+      title: {
+        text: 'Tweet count',
+        font: {
+          family: 'Courier New, monospace',
+          size: 18,
+          color: '#7f7f7f'
+        }
+      }
+    }
+  };
+
+
+  Plotly.newPlot('linechart', data, layout);
+}
+
+/**
+ * Bubble Chart
+ */
+function bubbleChart(bubbleData) {
+  var bubble_size = bubbleData.map(t => t.TweetCount / 18);
+  var bubble_count = bubbleData.map(t => t.TweetCount);
+
+  var trace = {
+    x: bubbleData.map(t => t.Airline),
+    y: bubbleData.map(t => t.WeekDay),
+    text: bubble_count,
+    // hoverinfo: 'hovertext',
+    mode: 'markers',
+    marker: {
+      size: bubble_size,
+      color: bubble_size
+    }
+  };
+
+  var data = [trace];
+
+  // console.log(data);
+
+  var layout = {
+    showlegend: false,
+    height: 550,
+    width: 700,
+    xaxis: { title: "Airline" },
+    yaxis: { title: "Week Day" }
+  };
+
+  Plotly.newPlot('bubblechart', data, layout);
+}
+
+/**
+ * Complete the click handler for the form
+ */
+submit.on("click", function () {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // airline
+  var airline = d3.select("#airline");
+  var airlineValue = airline.property("value");
+
+  // tweet
+  var tweet = d3.select("#tweet");
+  var tweetValue = tweet.property("value");
+
+  apiCall(airlineValue, tweetValue);
+});
+
+
+/**
+ * Populate grid
+ */
+function populateData(filteredData) {
+  /*
+  // Adds new rows of data for each tweet.
+  */
+  table.attr("class", "table table-striped hide in fade");
+  d3.select("#alertError").attr("class", "alert alert-danger alert-dismissable hide in fade");
+  d3.select("#alertSuccess").attr("class", "alert alert-success alert-dismissable hide in fade");
+  tbody.html('');
+
+  if (filteredData.length == 0) {
+    d3.select("#alertError").attr("class", "alert alert-danger alert-dismissable");
+    d3.select("#alertErrorText").text("No data found.")
+    return;
+  }
+
+  table.attr("class", "table table-striped");
+  d3.select("#alertSuccess").attr("class", "alert alert-success alert-dismissable");
+  d3.select("#alertSuccessText").text(`${filteredData.length} record(s) found.`);
+
+  $("#tweet-table").dataTable().fnDestroy();
+
+  filteredData.forEach((tweet) => {
+    var row = tbody.append("tr");
+    var cell = row.append("td");
+    cell.text(tweet.airline);
+    var cell = row.append("td");
+    cell.text(tweet.tweet);
+    var cell = row.append("td");
+    cell.text(tweet.sentiment);
+    var cell = row.append("td");
+    cell.text(tweet.date);
+    var cell = row.append("td");
+    cell.text(tweet.lat);
+    var cell = row.append("td");
+    cell.text(tweet.lng);
+  });
+
+  $('#tweet-table').dataTable({
+    destroy: true,
+    pageLength: 25,
+    paging: true,
+
+  });
+}
+
+init();
